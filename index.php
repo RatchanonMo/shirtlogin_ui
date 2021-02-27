a7641926f4bd658c3a15cfa806bac3d4d64f0a62
RatchanonMo <48505@yupparaj.ac.th>
2021-02-27 18:12:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>:: YRC Vote Shirt 2021 | ระบบโหวตเสื้อคณะสี ประจำปีการศึกษา 2564 โรงเรียนยุพราชวิทยาลัย จังหวัดเชียงใหม่ ::</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/animate.css">
+    <script src="./assets/js/wow.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <style>
+        .container-fluid {
+            max-width: 700px;
+        }
+    </style>
+</head>
+
+<body style="font-family: 'Prompt', sans-serif;background-image:url(./assets/img/bg.jpg)">
+
+    <div class="col-xl-12 mt-5 d-none d-xl-block">
+        <div class="container-fluid p-xl-5 p-sm-3 wow slideInLeft" style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;border-radius:10px;background-color:white">
+        <p align="center">
+        <img class="img-fluid" src="assets/img/logo.png" width="30%" alt="">
+        
+        </p>
+            <h3 class="d-none d-xl-block" align="center" style="color:#e91e63"><strong>ระบบโหวตเสื้อคณะสี ประจำปีการศึกษา 2564</strong></h3>
+            <h4 class="d-xl-none" align="center" style="color:#e91e63"><strong>ระบบโหวตเสื้อคณะสี <br>ประจำปีการศึกษา 2564</strong></h4>
+
+            <h4 class="d-none d-xl-block" align="center" class="mb-5">โรงเรียนยุพราชวิทยาลัย จังหวัดเชียงใหม่</h4>
+            <p class="d-xl-none" align="center" class="mb-5">โรงเรียนยุพราชวิทยาลัย จังหวัดเชียงใหม่</p>
+           
+            <div class="container mt-xl-4 mb-4">
+            <input type="text" class="form-control p-xl-3 mb-xl-4" placeholder="เลขบัตรประจำตัวประชาชน" style="background-color:#f8f9fa">
+            <input type="text" class="form-control p-xl-3 mb-xl-4" placeholder="เลขท้าย 5 ตัวบัตรประจำตัวประชาชน" style="background-color:#f8f9fa">
+
+           
+            <input type="submit" class="form-control btn btn-success p-xl-3 mb-2" value="เข้าสู่ระบบ">
+            <a href="" class="form-control btn btn-light  p-xl-3 " >รายงานผลโหวต</a>
+           
+         
+
+            </div>
+            <hr>
+            <p align="center" style="color:#929292">©2021 All Right Reserved Kampanart.Ch , Ratchanon.Mo , Kru Viratchai.Ju
+<br>Youth Computer Gen 9 Yupparaj Wittayalai School</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <div class="col-xl-12 mt-5 p-2 d-xl-none">
+        <div class="container-fluid p-xl-5 p-sm-3 wow pt-4" style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;border-radius:10px;background-color:white">
+        <p align="center">
+        <img class="img-fluid" src="assets/img/logo.png" width="30%" alt="">
+        
+        </p>
+          
+            <h4 class="d-xl-none" align="center" style="color:#e91e63"><strong>ระบบโหวตเสื้อคณะสี <br>ประจำปีการศึกษา 2564</strong></h4>
+
+            
+            <p class="d-xl-none" align="center" class="mb-5">โรงเรียนยุพราชวิทยาลัย จังหวัดเชียงใหม่</p>
+           
+            <div class="container mt-4 mb-4">
+            <input type="text" class="form-control p-xl-3 mb-2" placeholder="เลขบัตรประจำตัวประชาชน" style="background-color:#f8f9fa">
+
+            <input type="text" class="form-control p-xl-3 mb-2" placeholder="เลขท้าย 5 ตัวบัตรประจำตัวประชาชน" style="background-color:#f8f9fa">
+
+           
+            <input type="submit" class="form-control btn btn-success p-xl-3 mb-2" value="เข้าสู่ระบบ">
+            <a href="" class="form-control btn btn-light  p-xl-3 " >รายงานผลโหวต</a>
+           
+         
+
+            </div>
+            <hr>
+            <p class="pb-4" align="center" style="color:#929292;font-size:smaller">©2021 All Right Reserved Kampanart.Ch , Ratchanon.Mo , Kru Viratchai.Ju
+<br>Youth Computer Gen 9 Yupparaj Wittayalai School</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
